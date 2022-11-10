2e7ceffcbf6037488b60da1dc08a8b71a3d90c42
Valeria <valemarchisio@live.com.ar>
2022-11-10 13:28:19 -0300

    segundocuatri

--- segundoCuatri/clase3/ejercicio.ts
+++ segundoCuatri/clase3/ejercicio.ts
@@ -1,1 +1,20 @@
-console.log ("Hola mundo");
+class Animal{
+    cantidadDePatas : number;
+    nombre: string;
+    mostrarInfo(){
+        console.log("La info es:")
+    }
+    constructor(cantidadDePatas:number,nombre:string){
+        this.cantidadDePatas = cantidadDePatas;
+        this.nombre = nombre;
+    }
+}
+let araña = new Animal(8,"Araña");
+console.log(araña);
+
+
+
+
+
+
+
